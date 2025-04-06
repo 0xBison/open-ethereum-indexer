394bd9d149102b02a09d1915694eee2d42cf5dfc
0xBison <196279375+0xBison@users.noreply.github.com>
2025-04-06 18:11:41 +0800

    feat: add proxy contract example

---
+++ examples/proxy-contract/src/entity-registration.ts
@@ -0,0 +1,12 @@
+import { entityRegistry } from '@open-ethereum/indexer';
+import * as entities from './output/entities';
+
+// Register all entities from the entities object
+Object.keys(entities).forEach((key) => {
+  const entity = entities[key];
+  if (entity && typeof entity === 'function') {
+    entityRegistry.register(entity);
+  } else {
+    console.warn(`Skipping invalid entity export: ${key}`);
+  }
+});
