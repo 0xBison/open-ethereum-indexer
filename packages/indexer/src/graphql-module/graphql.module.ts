b97693cc4a1f0b5fa825cc5cc7cfb1308007d103
0xBison <196279375+0xBison@users.noreply.github.com>
2025-04-16 16:27:17 +0800

    feat: add support for custom entities in graphql

--- packages/indexer/src/graphql-module/graphql.module.ts
+++ packages/indexer/src/graphql-module/graphql.module.ts
@@ -4,6 +4,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { createGenericEntityResolver } from './generic-entity.resolver.factory';
+import { createCustomEntityResolver } from './custom-entity.resolver.factory';
 import { entityRegistry } from '../generic-indexer-module/entity-registry';
 import { BlockIndex } from '../database-module/core/BlockIndex.entity';
 import { BlockIndexResolver } from './block.resolver';
@@ -13,16 +14,30 @@ export class GraphQLAppModule {
   static forRoot(config?: { disablePlayground?: boolean }): DynamicModule {
     const allEntities = entityRegistry.getAll();
 
-    const blockchainEventEntities = allEntities.filter((entity) => {
+    // Split entities into generic and custom
+    const genericEntities = allEntities.filter((entity) => {
       const prototype = Object.getPrototypeOf(entity.prototype);
       return (
         prototype && prototype.constructor.name === 'BlockchainEventEntity'
       );
     });
 
-    const resolvers = blockchainEventEntities.map((entity) => {
+    const customEntities = allEntities.filter((entity) => {
+      const prototype = Object.getPrototypeOf(entity.prototype);
+      return (
+        !prototype || prototype.constructor.name !== 'BlockchainEventEntity'
+      );
+    });
+
+    // Create resolvers for both types
+    const genericResolvers = genericEntities.map((entity) => {
       const eventName = entity.name.replace('Entity', '');
       return createGenericEntityResolver(entity, eventName);
+    });
+
+    const customResolvers = customEntities.map((entity) => {
+      const entityName = entity.name.replace('Entity', '');
+      return createCustomEntityResolver(entity, entityName);
     });
 
     return {
@@ -34,9 +49,9 @@ export class GraphQLAppModule {
           sortSchema: true,
           playground: !config?.disablePlayground,
         }),
-        TypeOrmModule.forFeature([...blockchainEventEntities, BlockIndex]),
+        TypeOrmModule.forFeature([...allEntities, BlockIndex]),
       ],
-      providers: [...resolvers, BlockIndexResolver],
+      providers: [...genericResolvers, ...customResolvers, BlockIndexResolver],
       exports: [GraphQLModule],
     };
   }
